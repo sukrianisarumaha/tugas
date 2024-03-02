4ef9b98b6e52f17ec586ba86d072abaee630ae43
sukrianisarumaha <161433305+sukrianisarumaha@users.noreply.github.com>
2024-03-03 04:53:48 +0700

    Add files via upload

---
+++ cVKdanTD.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sukri
+ */
+public class cVKdanTD {
+    
+    public static void main(String[] args){
+        
+        //VARIABEL
+           //tipedata namavariabel;
+           
+          //byte x;
+          //x = 10;
+          
+          //char x;
+          //x = 120;
+          
+          //double x;
+          // x = 5/2;
+          
+          //boolean x;
+          //x = true;
+          //x = false;
+          
+          //int x = 50;
+          
+          //KONSTANTA
+           final int x = 20;
+           
+           
+           
+           System.out.print(x);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
